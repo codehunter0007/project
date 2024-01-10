84fac42044c5f7be34b45dc776191606ab44b183
FUTUNN\fly <fly@futunn.com>
2024-01-10 19:09:38 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,14 @@
 <template>
   <div>
-    <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
-    <el-button type="success" size="small" :icon="Edit">编辑</el-button>
-    <el-button type="danger" size="smdefaultall" :icon="Delete">
-      删除按钮
-    </el-button>
-    <el-pagination
-      :page-size="100"
-      layout="total, prev, pager, next"
-      :total="1000"
-    ></el-pagination>
-  </div>
-  <div class="box">
-    <h1>str</h1>
+     <h1>svg测试</h1>
+     <svg-icon name="phone" color="pink" width="100px" height="100px"></svg-icon>
   </div>
 </template>
 
 <script setup lang="ts">
-//引入图标组件
-import { Plus, Edit, Delete } from '@element-plus/icons-vue'
-
-for (let i = 0; i < 100; i++) {
-  console.log(i)
-}
+import SvgIcon from '@/components/SvgIcon/index.vue'
 </script>
 
 <style scoped>
-.box {
-  width: 600px;
-  height: 400px;
-  background: black;
-  h1 {
-    color: white;
-  }
-}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 //引入element-plus插件与样式
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
@@ -11,12 +11,7 @@ const app = createApp(App)
 //安装element-plus插件
 app.use(ElementPlus, {
   locale: zhCn, //element-plus国际化配置
-})
+});
+import 'virtual:svg-icons-register'
 // 将应用挂载到挂载点上
 app.mount('#app')
-
-const fun = () => {
-  console.log('我爱你祖国')
-}
-
-fun()
